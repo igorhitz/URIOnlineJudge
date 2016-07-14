fa4f8f992f0bef42a6465b5ffc8d61976dc305bc
igorhitz <igorhitz@gmail.com>
2016-07-14 14:25:21 -0300

    Problema 1023

--- Estruturas e Bibliotecas/1023.py
+++ Estruturas e Bibliotecas/1023.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from collections import Counter
 
 def truncate(f, n):
     '''Truncates/pads a float f to n decimal places without rounding'''
@@ -6,6 +7,25 @@ def truncate(f, n):
     i, p, d = s.partition('.')
     return '.'.join([i, (d+'0'*n)[:n]])
 
+def countingsort(l, k):
+
+	k = k + 1
+	counter = [0] * k
+	for x in l:
+		counter[x] += 1
+	
+	total = 0
+	for i in range(k):
+		oldCount = counter[i]
+		counter[i] = total
+		total += oldCount
+
+	output = [0] * len(l)
+	for x in l:
+		output[counter[x]] = (x,l[x])
+		counter[x] += 1
+
+	return output
 
 contador = 0
 n = input()
@@ -18,8 +38,8 @@ while n != 0:
 		morador,totalconsumo = raw_input().split()
 		morador = int(morador)
 		totalconsumo = int(totalconsumo)
-		media = totalconsumo/morador
-		if media in cidade:
+		media = int(totalconsumo/morador)
+		if cidade.get(media) != None:
 			cidade[media] += morador
 		else:
 			cidade[media] = morador
@@ -32,11 +52,14 @@ while n != 0:
 	else:
 		print "Cidade# %d:" % (contador)
 	string = ""
-	for key in sorted(cidade):
-		string += "%d-%d " % (cidade[key], key)
+	cidadeSorted = countingsort(cidade, 200)
+	for i in cidadeSorted:
+	    string += "%d-%d" % (i[1], i[0])
+	    if i != cidadeSorted[-1]:
+	        string += " "
 	print string
 	mediafinal = truncate(mediacidade, 2)
-	print "Consumo medio: %s m3" % (mediafinal)
+	print "Consumo medio: %s m3." % (mediafinal)
 
 
 	cidade.clear()
